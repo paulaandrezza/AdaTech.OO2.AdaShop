0e3b864eb0d750b2b166ce1f48ae04520b79ed31
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-10 17:16:24 -0300

    Add InfoPayment atributes and Enums

---
+++ AdaTech.AdaShop/AdaTech.AdaShop.Domain/Models/Enum/PaymentMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaTech.AdaShop.Domain.Models.Enum
+{
+    public enum PaymentMethods
+    {
+        CreditCard = 1,
+        DebitCard,
+        Pix,
+        BankSlip,
+    }
+}
--- AdaTech.AdaShop/AdaTech.AdaShop.Domain/Models/Order/InfoPayments.cs
+++ AdaTech.AdaShop/AdaTech.AdaShop.Domain/Models/Order/InfoPayments.cs
@@ -1,7 +1,11 @@
-﻿namespace AdaTech.AdaShop.Domain.Models.Order
+﻿using AdaTech.AdaShop.Domain.Models.Enum;
+
+namespace AdaTech.AdaShop.Domain.Models.Order
 {
     public class InfoPayments
     {
         public string ID { get; set; }
+        public PaymentMethods PaymentMethod { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
     }
 }
