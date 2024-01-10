aedb22bd5ab3cb75af797bc3022ad16ea17d0164
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-10 17:21:21 -0300

    feat: add description to product

--- AdaTech.AdaShop/AdaTech.AdaShop.Domain/Models/Order/Product.cs
+++ AdaTech.AdaShop/AdaTech.AdaShop.Domain/Models/Order/Product.cs
@@ -4,15 +4,17 @@ namespace AdaTech.AdaShop.Domain.Models.Order
 {
     public class Product : IEntity
     {
-        public Product(string name, decimal price)
+        public Product(string name, decimal price, string? description)
         {
             ID = Guid.NewGuid().ToString();
             Name = name;
             Price = price;
+            Description = description;
         }
 
         public string ID { get; private set; }
         public string Name { get; private set; }
         public decimal Price { get; private set; }
+        public string? Description { get; set; }
     }
 }
