df9da8335fd9320af7e6e733c6ed83f0a1e85ee0
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-10 17:29:25 -0300

    feat: add InfoPayments constructor

--- AdaTech.AdaShop/AdaTech.AdaShop.Domain/Models/Order/InfoPayments.cs
+++ AdaTech.AdaShop/AdaTech.AdaShop.Domain/Models/Order/InfoPayments.cs
@@ -7,5 +7,11 @@ namespace AdaTech.AdaShop.Domain.Models.Order
         public string ID { get; set; }
         public PaymentMethods PaymentMethod { get; set; }
         public PaymentStatus PaymentStatus { get; set; }
+
+        public InfoPayments(PaymentMethods paymentMethod) {
+            ID = Guid.NewGuid().ToString();
+            PaymentMethod = paymentMethod;
+            PaymentStatus = PaymentStatus.Pending;
+        }
     }
 }
